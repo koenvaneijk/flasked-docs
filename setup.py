63cf5a62062ddb70edda92f27f4aaa1cd07d5e5c
Koen van Eijk <vaneijk.koen@gmail.com>
2022-12-09 12:04:07 +0100

    Getting there... final changes to MANIFEST.in and setup.py to get the templates and static files included with the package

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="Flasked-Docs",
-    version="0.0.4",
+    version="0.0.5",
     packages=["flasked_docs"],
     url="",
     license="AGPLv3",
     author="Koen van Eijk",
     author_email="vaneijk.koen@gmail.com",
     install_requires=["flask", "markdown"],
+    include_package_data=True,
 )
