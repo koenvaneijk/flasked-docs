02de610e67732b20c3a00a477713f8386c4abbbe
Koen van Eijk <vaneijk.koen@gmail.com>
2022-12-09 12:00:27 +0100

    Added MANIFEST.in file and removed package_data from setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name="Flasked-Docs",
-    version="0.0.3",
+    version="0.0.4",
     packages=["flasked_docs"],
     url="",
     license="AGPLv3",
     author="Koen van Eijk",
     author_email="vaneijk.koen@gmail.com",
     install_requires=["flask", "markdown"],
-    package_data={"flasked_docs": ["templates/*", "static/*"]},
 )
