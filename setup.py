90dcef6d2b12b98d5063298d85f8ef8367e4567a
Koen van Eijk <vaneijk.koen@gmail.com>
2022-12-09 11:55:19 +0100

    Added package_data to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="Flasked-Docs",
-    version="0.0.2",
+    version="0.0.3",
     packages=["flasked_docs"],
     url="",
     license="AGPLv3",
     author="Koen van Eijk",
     author_email="vaneijk.koen@gmail.com",
     install_requires=["flask", "markdown"],
+    package_data={"flasked_docs": ["templates/*", "static/*"]},
 )
