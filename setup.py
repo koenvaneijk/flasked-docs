ab01dcf65e9f405f4683086e6d22625d7ee84c5a
Koen van Eijk <vaneijk.koen@gmail.com>
2022-12-09 11:50:08 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="Flasked-Docs",
-    version="0.0.1",
+    version="0.0.2",
     packages=["flasked_docs"],
     url="",
     license="AGPLv3",
     author="Koen van Eijk",
     author_email="vaneijk.koen@gmail.com",
-    requires=["flask", "markdown"],
+    install_requires=["flask", "markdown"],
 )
